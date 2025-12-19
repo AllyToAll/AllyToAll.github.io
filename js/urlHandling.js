8227e5f95e4a730bf782ea5e5e79cdc8783792ff
AllyToAll <allytoall@protonmail.com>
2025-12-19 14:38:27 +0000

    Better urlHandling.js

--- js/urlHandling.js
+++ js/urlHandling.js
@@ -1,9 +1,16 @@
-let pathname = window.location.pathname;
-if (pathname !== '/' && pathname.endsWith('/')) {
-    window.history.replaceState({}, '', pathname.slice(0, -1));
-    pathname = pathname.slice(0, -1);
-}
-if (pathname.endsWith('/index.html')) {
-    const cleanPath = pathname.replace(/\/index\.html$/, '') || '/';
-    window.location.replace(cleanPath);
+const isLocalhost = location.hostname === 'localhost' || location.hostname === '127.0.0.1';
+
+if (!isLocalhost) {
+    const {pathname, search, hash} = window.location;
+    let newPath = pathname;
+    if (newPath.length > 1 && newPath.endsWith('/')) {
+        newPath = newPath.slice(0, -1);
+    }
+    if (newPath.endsWith('/index.html')) {
+        newPath = newPath.replace(/\/index\.html$/, '') || '/';
+    }
+    const newUrl = newPath + search + hash;
+    if (newUrl !== pathname + search + hash) {
+        window.location.replace(newUrl);
+    }
 }
