2777b66b32004c8e44d73a2ba48bf0a48b68ebad
AllyToAll <allytoall@protonmail.com>
2026-01-16 12:38:05 +0000

    Improve studies

--- scripts/tailwind.bash
+++ scripts/tailwind.bash
@@ -1,1 +1,2 @@
-npx @tailwindcss/cli -i ./css/style.css -o ./css/output.css --watch
+npx @tailwindcss/cli -i ../css/style.css -o ../css/output.css
+npx @tailwindcss/cli -i ../css/studies.css -o ../css/output-studies.css
